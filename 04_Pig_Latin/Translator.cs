ce7fc2a8fc01089a833004e9e01716d5be50622a
casp4 <casp4@LAPTOP-O5C9AQD0>
2016-09-13 10:50:29 +0200

    Add project files.

---
+++ 04_Pig_Latin/Translator.cs
@@ -0,0 +1,73 @@
+﻿namespace _04_Pig_Latin
+{
+    internal class Translator
+    {
+        public string Translate(string s)
+        {
+            string[] a = s.Split(' ');
+            int i = 0;
+            string res = "";
+            foreach (string item in a)
+            {
+
+                if (item.Contains("qu"))
+                {
+                    i = CheckForQu(item);
+                }
+                else
+                {
+                    i = CheckForCharsBeforeVowel(item);
+                }
+
+                if (i == 0)
+                {
+                    res = res + " " + item + "ay";
+                }
+                else
+                {
+                    res = res + " " + item.Substring(i) + item.Substring(0, i) + "ay";
+                }
+            }
+
+            return res.TrimStart();
+
+        }
+
+        public int CheckForQu(string s)
+        {
+            int i = 1;
+            foreach (char c in s)
+            {
+                if (c == 'u')
+                {
+                    break;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return i;
+        }
+
+        public int CheckForCharsBeforeVowel(string s)
+        {
+            int i = 0;
+            foreach (char c in s)
+            {
+                bool isVowel = "aeiouAEIOU".IndexOf(c) >= 0;
+                if (isVowel)
+                {
+                    break;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return i;
+        }
+
+    }
+}
